a4f37034340cb4d6c8bc74fd773059e6918587b2
renming <renming1414@sina.com>
2019-04-26 18:18:32 +0800

    file_detect:     add jar com.renm.cron and almost finish FileCheck.java     add related dependence in pom.xml     project file(file_detect.iml) changed by intellij

---
+++ java/file_detect/src/main/java/com/renm/cron/FileCheckCron.java
@@ -0,0 +1,17 @@
+package com.renm.cron;
+
+import com.renm.pojo.FileCron;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+
+public class FileCheckCron {
+    public void add(FileCron fileCron) {
+
+    }
+
+    public void add(List<FileCron> fileCronList) {
+
+    }
+}
